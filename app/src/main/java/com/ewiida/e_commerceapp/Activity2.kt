d46fd108a85c6a80bbe93992cc87a20e1b2331c7
mahmoud ewida <mahmoud.ewida9900@gmail.com>
2022-09-06 12:45:37 +0200

    bottom nav

--- app/src/main/java/com/ewiida/e_commerceapp/Activity2.kt
+++ app/src/main/java/com/ewiida/e_commerceapp/Activity2.kt
@@ -3,20 +3,70 @@ package com.ewiida.e_commerceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.ewiida.e_commerceapp.databinding.Activity2Binding
+import com.ewiida.e_commerceapp.mainScreens.Cart
+import com.ewiida.e_commerceapp.mainScreens.Home
+import com.ewiida.e_commerceapp.mainScreens.Notifications
+import com.ewiida.e_commerceapp.mainScreens.Profile
+import com.google.android.material.navigation.NavigationBarView
 
 class Activity2 : AppCompatActivity() {
+    lateinit var binding : Activity2Binding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = Activity2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity2)
-
-        sayFuck()
+        setContentView(binding.root)
+        var home=Home()
+        replaceFragment(home)
+        BottomNavigationBarControler()
+        hideBars()
     }
 
-    private fun sayFuck(){
-        Log.i("hi", "hi mahmoud")
+
+
+
+    private fun BottomNavigationBarControler(){
+        binding.bottomNavigationView.setOnItemSelectedListener(object :NavigationBarView.OnItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId){
+                    R.id.menuItemHome ->{
+                        var home=Home()
+                        replaceFragment(home)
+
+                    }
+                    R.id.menuItemCart ->{
+                        var cart=Cart()
+                        replaceFragment(cart)
+                    }
+                    R.id.menuItemNotifications ->{
+                        var noti=Notifications()
+                        replaceFragment(noti)
+                    }
+                    R.id.menuItemProfile ->{
+                        var profile=Profile()
+                        replaceFragment(profile)
+                    }
+                }
+                return true
+            }
+        })
+
     }
 
-    fun sayhi(){
-        Log.d("tag","hi")
+    private fun replaceFragment(f:Fragment){
+        var fm=supportFragmentManager
+        var ft=fm.beginTransaction()
+        ft.replace(R.id.container,f)
+        ft.commit()
+    }
+
+    private fun hideBars(){
+        window.decorView.apply {
+            systemUiVisibility= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
     }
 }
