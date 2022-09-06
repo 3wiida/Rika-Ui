2ace9e8132bb7da789fcfc224bbc65f3298aa376
mahmoud ewida <mahmoud.ewida9900@gmail.com>
2022-09-06 09:45:31 +0200

    test #3

--- app/src/main/java/com/ewiida/e_commerceapp/Activity2.kt
+++ app/src/main/java/com/ewiida/e_commerceapp/Activity2.kt
@@ -2,10 +2,15 @@ package com.ewiida.e_commerceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
+    }
+
+    fun sayhi(){
+        Log.d("tag","hi")
     }
 }
